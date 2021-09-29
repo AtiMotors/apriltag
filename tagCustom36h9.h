7df9566100dc908886b9b9f517c4f87ec00cd7c0
arin07 <arindamdhar07@gmail.com>
2021-09-29 05:47:48 +0000

    Added custom tag

---
+++ tagCustom36h9.h
@@ -0,0 +1,17 @@
+#ifndef _TAGCustom36H9
+#define _TAGCustom36H9
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tagCustom36h9_create();
+void tagCustom36h9_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
